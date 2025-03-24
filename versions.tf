ca91052125b671ecb49b4e5e3ec4c8688e12f181
Zeljko Bekcic <zeljko.bekcic@iits-consulting.de>
2025-03-24 16:34:51 +0100

    add: github actions, correct versions, correct readme, license

--- versions.tf
+++ versions.tf
@@ -1,8 +1,11 @@
 terraform {
+
+  required_version = ">= 1.4.0"
+
   required_providers {
     opentelekomcloud = {
       source  = "opentelekomcloud/opentelekomcloud"
-      version = ">=1.31.5"
+      version = "~> 1.32"
     }
   }
 }
