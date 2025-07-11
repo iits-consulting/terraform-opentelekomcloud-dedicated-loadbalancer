b36d7184c1e533f14e7c4fbf1abb5009006e40f8
canaykin <70644420+canaykin@users.noreply.github.com>
2025-07-11 15:58:57 +0200

    allow disabling EIP for private loadbalancing
    feat(eip): allow disabling EIP for private loadbalancing
    feat(ipbackends): exposed the loadbalancer flag for enabling ip as backend feature as input
    feat(naming): renamed elb resources to loadbalancer to better differentiate with shared elb resources

--- main.tf
+++ main.tf
@@ -8,19 +8,25 @@ data "opentelekomcloud_lb_flavor_v3" "layer7_flavor" {
   name  = var.layer7_flavor
 }
 
-resource "opentelekomcloud_lb_loadbalancer_v3" "elb" {
-  name        = "${var.name_prefix}-ELB"
-  description = "Dedicated ELB instance with public IP."
-  subnet_id   = var.subnet_id
-  network_ids = var.network_ids
+resource "opentelekomcloud_lb_loadbalancer_v3" "loadbalancer" {
+  name             = "${var.name_prefix}-lb"
+  description      = "Dedicated loadbalancer instance with public IP."
+  subnet_id        = var.subnet_id
+  network_ids      = var.network_ids
+  ip_target_enable = var.ip_target_enable
 
   l4_flavor = length(var.layer4_flavor) == 0 ? null : data.opentelekomcloud_lb_flavor_v3.layer4_flavor[0].id
   l7_flavor = length(var.layer7_flavor) == 0 ? null : data.opentelekomcloud_lb_flavor_v3.layer7_flavor[0].id
 
   availability_zones = var.availability_zones
-  public_ip {
-    id = opentelekomcloud_vpc_eip_v1.ingress_eip.id
+
+  dynamic "public_ip" {
+    for_each = opentelekomcloud_vpc_eip_v1.ingress_eip
+    content {
+      id = public_ip.value.id
+    }
   }
+
   tags = var.tags
   lifecycle {
     ignore_changes = [router_id]
@@ -28,9 +34,10 @@ resource "opentelekomcloud_lb_loadbalancer_v3" "elb" {
 }
 
 resource "opentelekomcloud_vpc_eip_v1" "ingress_eip" {
+  count = var.bandwidth == 0 ? 0 : 1
   bandwidth {
     charge_mode = "traffic"
-    name        = "${var.name_prefix}-ELB"
+    name        = "${var.name_prefix}-lb"
     share_type  = "PER"
     size        = var.bandwidth
   }
